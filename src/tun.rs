b83d1ddd274ef3de9977eb98db1cbe75fe39e913
Lee Smet <lee.smet@hotmail.com>
2023-08-25 16:13:48 +0200

    Delete obsolete IpPacketCodec
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- src/tun.rs
+++ src/tun.rs
@@ -29,64 +29,6 @@ struct IpPacketCodec {
     _p: (),
 }
 
-impl IpPacketCodec {
-    /// Instantiate a new codec.
-    fn new() -> Self {
-        Self { _p: () }
-    }
-}
-
-impl Encoder<IpPacket> for IpPacketCodec {
-    // std::convert::Infallible does not implement Into<std::io::Error> so we can't use that here.
-    type Error = io::Error;
-
-    fn encode(&mut self, item: IpPacket, dst: &mut bytes::BytesMut) -> Result<(), Self::Error> {
-        Ok(dst.put_slice(&item.0))
-    }
-}
-
-impl Decoder for IpPacketCodec {
-    type Item = IpPacket;
-
-    type Error = io::Error;
-
-    fn decode(&mut self, src: &mut bytes::BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        // Only try to decode in case we actually have sufficient data for a header: 20 bytes (in
-        // case of IPv4)
-        if src.remaining() < 20 {
-            return Ok(None);
-        }
-
-        // Decode a header, this does not advance the buffer yet.
-        let (header, _, _) = match etherparse::IpHeader::from_slice(&src) {
-            Ok(h) => h,
-            Err(ReadError::UnexpectedEndOfSlice(_)) => return Ok(None),
-            Err(_) => {
-                return Err(io::Error::new(
-                    io::ErrorKind::InvalidData,
-                    "can't parse IP header",
-                ))
-            }
-        };
-
-        //   .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "can't parse IP header"))?;
-        let size = match header {
-            IpHeader::Version4(hdr, _) => hdr.total_len(),
-            IpHeader::Version6(hdr, _) => hdr.payload_length + hdr.header_len() as u16,
-        } as usize;
-
-        if src.remaining() < size {
-            return Ok(None);
-        }
-
-        let mut packet = vec![0; size];
-        packet.copy_from_slice(&src[..size]);
-        src.advance(size);
-
-        Ok(Some(IpPacket(packet)))
-    }
-}
-
 impl Deref for IpPacket {
     type Target = [u8];
 
