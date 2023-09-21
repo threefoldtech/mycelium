49f1b1cbc3f9700051921d46c443d0472018d41c
Lee Smet <lee.smet@hotmail.com>
2023-09-21 15:12:39 +0200

    Fix data packet source IP transmission
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- src/packet/data.rs
+++ src/packet/data.rs
@@ -6,22 +6,22 @@ use tokio_util::codec::{Decoder, Encoder};
 #[derive(Debug, Clone)]
 pub struct DataPacket {
     pub raw_data: Vec<u8>, // eccrypte data isself then append the nonce
-    pub dst_ip: Ipv6Addr,
     pub src_ip: Ipv6Addr,
+    pub dst_ip: Ipv6Addr,
 }
 
 pub struct Codec {
     len: Option<u16>,
-    dest_ip: Option<Ipv6Addr>,
     src_ip: Option<Ipv6Addr>,
+    dest_ip: Option<Ipv6Addr>,
 }
 
 impl Codec {
     pub fn new() -> Self {
         Codec {
             len: None,
-            dest_ip: None,
             src_ip: None,
+            dest_ip: None,
         }
     }
 }
@@ -46,24 +46,6 @@ impl Decoder for Codec {
             data_len
         } as usize;
 
-        // Determine the destination IP
-        let dest_ip = if let Some(dest_ip) = self.dest_ip {
-            dest_ip
-        } else {
-            if src.len() < 16 {
-                return Ok(None);
-            }
-
-            // Decode octets
-            let mut ip_bytes = [0u8; 16];
-            ip_bytes.copy_from_slice(&src[..16]);
-            let dest_ip = Ipv6Addr::from(ip_bytes);
-            src.advance(16);
-
-            self.dest_ip = Some(dest_ip);
-            dest_ip
-        };
-
         // Determine the source IP
         let src_ip = if let Some(src_ip) = self.src_ip {
             src_ip
@@ -80,6 +62,24 @@ impl Decoder for Codec {
 
             self.src_ip = Some(src_ip);
             src_ip
+        };
+
+        // Determine the destination IP
+        let dest_ip = if let Some(dest_ip) = self.dest_ip {
+            dest_ip
+        } else {
+            if src.len() < 16 {
+                return Ok(None);
+            }
+
+            // Decode octets
+            let mut ip_bytes = [0u8; 16];
+            ip_bytes.copy_from_slice(&src[..16]);
+            let dest_ip = Ipv6Addr::from(ip_bytes);
+            src.advance(16);
+
+            self.dest_ip = Some(dest_ip);
+            dest_ip
         };
 
         // Check we have enough data to decode
@@ -112,9 +112,9 @@ impl Encoder<DataPacket> for Codec {
         dst.reserve(item.raw_data.len() + 2 + 16 + 32);
         // Write the length of the data
         dst.put_u16(item.raw_data.len() as u16);
+        // Write the source IP
+        dst.put_slice(&item.src_ip.octets());
         // Write the destination IP
-        dst.put_slice(&item.dst_ip.octets());
-        // Write the public key
         dst.put_slice(&item.dst_ip.octets());
         // Write the data
         dst.extend_from_slice(&item.raw_data);
