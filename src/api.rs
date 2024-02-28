22cb2c939c05e4853d8b6503e64a88610671c62a
Lee Smet <lee.smet@hotmail.com>
2024-02-28 14:40:39 +0100

    Move message API handlers to dedicated module
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- src/api.rs
+++ src/api.rs
@@ -1,29 +1,26 @@
 use std::{
-    net::{IpAddr, SocketAddr},
-    ops::Deref,
+    net::SocketAddr,
     str::FromStr,
     sync::{Arc, Mutex},
-    time::Duration,
 };
 
 use axum::{
-    extract::{Path, Query, State},
+    extract::{Path, State},
     http::StatusCode,
-    routing::{delete, get, post},
+    routing::{delete, get},
     Json, Router,
 };
 use log::{debug, error};
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    crypto::PublicKey,
     endpoint::Endpoint,
-    message::{MessageId, MessageInfo, MessageStack},
+    message::MessageStack,
     peer_manager::{PeerExists, PeerManager, PeerNotFound, PeerStats},
 };
 
-/// Default amount of time to try and send a message if it is not explicitly specified.
-const DEFAULT_MESSAGE_TRY_DURATION: Duration = Duration::from_secs(60 * 5);
+mod message;
+pub use message::{MessageDestination, MessageReceiveInfo, MessageSendInfo, PushMessageResponse};
 
 /// Http API server handle. The server is spawned in a background task. If this handle is dropped,
 /// the server is terminated.
@@ -42,51 +39,6 @@ struct HttpServerState {
     peer_manager: PeerManager,
     /// Access to messages.
     message_stack: MessageStack,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct MessageSendInfo {
-    pub dst: MessageDestination,
-    #[serde(default)]
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(with = "base64::optional_binary")]
-    pub topic: Option<Vec<u8>>,
-    #[serde(with = "base64::binary")]
-    pub payload: Vec<u8>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub enum MessageDestination {
-    Ip(IpAddr),
-    Pk(PublicKey),
-}
-
-#[derive(Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct MessageReceiveInfo {
-    pub id: MessageId,
-    pub src_ip: IpAddr,
-    pub src_pk: PublicKey,
-    pub dst_ip: IpAddr,
-    pub dst_pk: PublicKey,
-    #[serde(default)]
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(with = "base64::optional_binary")]
-    pub topic: Option<Vec<u8>>,
-    #[serde(with = "base64::binary")]
-    pub payload: Vec<u8>,
-}
-
-impl MessageDestination {
-    /// Get the IP address of the destination.
-    fn ip(self) -> IpAddr {
-        match self {
-            MessageDestination::Ip(ip) => ip,
-            MessageDestination::Pk(pk) => IpAddr::V6(pk.address()),
-        }
-    }
 }
 
 impl Http {
@@ -109,11 +61,7 @@ impl Http {
             .route("/admin/routes/selected", get(get_selected_routes))
             .route("/admin/routes/fallback", get(get_fallback_routes))
             .with_state(server_state.clone());
-        let msg_routes = Router::new()
-            .route("/messages", get(get_message).post(push_message))
-            .route("/messages/status/:id", get(message_status))
-            .route("/messages/reply/:id", post(reply_message))
-            .with_state(server_state);
+        let msg_routes = message::message_router_v1(server_state);
         let app = Router::new()
             .nest("/api/v1", msg_routes)
             .nest("/api/v1", admin_routes);
@@ -131,197 +79,6 @@ impl Http {
         });
         Http { _cancel_tx }
     }
-}
-
-#[derive(Deserialize)]
-struct GetMessageQuery {
-    peek: Option<bool>,
-    timeout: Option<u64>,
-    /// Optional filter for start of the message, base64 encoded.
-    #[serde(default)]
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(with = "base64::optional_binary")]
-    topic: Option<Vec<u8>>,
-}
-
-impl GetMessageQuery {
-    /// Did the query indicate we should peek the message instead of pop?
-    fn peek(&self) -> bool {
-        matches!(self.peek, Some(true))
-    }
-
-    /// Amount of seconds to hold and try and get values.
-    fn timeout_secs(&self) -> u64 {
-        self.timeout.unwrap_or(0)
-    }
-}
-
-async fn get_message(
-    State(state): State<HttpServerState>,
-    Query(query): Query<GetMessageQuery>,
-) -> Result<Json<MessageReceiveInfo>, StatusCode> {
-    debug!(
-        "Attempt to get message, peek {}, timeout {} seconds",
-        query.peek(),
-        query.timeout_secs()
-    );
-
-    // A timeout of 0 seconds essentially means get a message if there is one, and return
-    // immediatly if there isn't. This is the result of the implementation of Timeout, which does a
-    // poll of the internal future first, before polling the delay.
-    tokio::time::timeout(
-        Duration::from_secs(query.timeout_secs()),
-        state.message_stack.message(!query.peek(), query.topic),
-    )
-    .await
-    .or(Err(StatusCode::NO_CONTENT))
-    .map(|m| {
-        Json(MessageReceiveInfo {
-            id: m.id,
-            src_ip: m.src_ip,
-            src_pk: m.src_pk,
-            dst_ip: m.dst_ip,
-            dst_pk: m.dst_pk,
-            topic: if m.topic.is_empty() {
-                None
-            } else {
-                Some(m.topic)
-            },
-            payload: m.data,
-        })
-    })
-}
-
-#[derive(Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct MessageIdReply {
-    id: MessageId,
-}
-
-#[derive(Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-#[serde(untagged)]
-pub enum PushMessageResponse {
-    Reply(MessageReceiveInfo),
-    Id(MessageIdReply),
-}
-
-#[derive(Deserialize)]
-struct PushMessageQuery {
-    reply_timeout: Option<u64>,
-}
-
-impl PushMessageQuery {
-    /// The user requested to wait for the reply or not.
-    fn await_reply(&self) -> bool {
-        self.reply_timeout.is_some()
-    }
-
-    /// Amount of seconds to wait for the reply.
-    fn timeout(&self) -> u64 {
-        self.reply_timeout.unwrap_or(0)
-    }
-}
-
-async fn push_message(
-    State(state): State<HttpServerState>,
-    Query(query): Query<PushMessageQuery>,
-    Json(message_info): Json<MessageSendInfo>,
-) -> Result<(StatusCode, Json<PushMessageResponse>), StatusCode> {
-    let dst = message_info.dst.ip();
-    debug!(
-        "Pushing new message of {} bytes to message stack for target {dst}",
-        message_info.payload.len(),
-    );
-
-    let (id, sub) = match state.message_stack.new_message(
-        dst,
-        message_info.payload,
-        if let Some(topic) = message_info.topic {
-            topic
-        } else {
-            vec![]
-        },
-        DEFAULT_MESSAGE_TRY_DURATION,
-        query.await_reply(),
-    ) {
-        Ok((id, sub)) => (id, sub),
-        Err(_) => {
-            return Err(StatusCode::BAD_REQUEST);
-        }
-    };
-
-    if !query.await_reply() {
-        // If we don't wait for the reply just return here.
-        return Ok((
-            StatusCode::CREATED,
-            Json(PushMessageResponse::Id(MessageIdReply { id })),
-        ));
-    }
-
-    let mut sub = sub.unwrap();
-    tokio::select! {
-        sub_res = sub.changed() => {
-            match sub_res {
-                Ok(_) => {
-                    if let Some(m) = sub.borrow().deref()  {
-                        Ok((StatusCode::OK, Json(PushMessageResponse::Reply(MessageReceiveInfo {
-                            id: m.id,
-                            src_ip: m.src_ip,
-                            src_pk: m.src_pk,
-                            dst_ip: m.dst_ip,
-                            dst_pk: m.dst_pk,
-                            topic: if m.topic.is_empty() { None } else { Some(m.topic.clone()) },
-                            payload: m.data.clone(),
-                        }))))
-                    } else {
-                        // This happens if a none value is send, which should not happen.
-                        Err(StatusCode::INTERNAL_SERVER_ERROR)
-                    }
-                }
-                Err(_)  => {
-                    // This happens if the sender drops, which should not happen.
-                    Err(StatusCode::INTERNAL_SERVER_ERROR)
-                }
-            }
-        },
-        _ = tokio::time::sleep(Duration::from_secs(query.timeout())) => {
-            // Timeout expired while waiting for reply
-            Ok((StatusCode::REQUEST_TIMEOUT, Json(PushMessageResponse::Id(MessageIdReply { id  }))))
-        }
-    }
-}
-
-async fn reply_message(
-    State(state): State<HttpServerState>,
-    Path(id): Path<MessageId>,
-    Json(message_info): Json<MessageSendInfo>,
-) -> StatusCode {
-    let dst = message_info.dst.ip();
-    debug!(
-        "Pushing new reply to {} of {} bytes to message stack for target {dst}",
-        id.as_hex(),
-        message_info.payload.len(),
-    );
-
-    state
-        .message_stack
-        .reply_message(id, dst, message_info.payload, DEFAULT_MESSAGE_TRY_DURATION);
-
-    StatusCode::NO_CONTENT
-}
-
-async fn message_status(
-    State(state): State<HttpServerState>,
-    Path(id): Path<MessageId>,
-) -> Result<Json<MessageInfo>, StatusCode> {
-    debug!("Fetching message status for message {}", id.as_hex());
-
-    state
-        .message_stack
-        .message_info(id)
-        .ok_or(StatusCode::NOT_FOUND)
-        .map(Json)
 }
 
 /// Get the stats of the current known peers
@@ -472,65 +229,6 @@ impl Serialize for Metric {
         match self {
             Self::Infinite => serializer.serialize_str("infinite"),
             Self::Value(v) => serializer.serialize_u16(*v),
-        }
-    }
-}
-
-/// Module to implement base64 decoding and encoding
-// Sourced from https://users.rust-lang.org/t/serialize-a-vec-u8-to-json-as-base64/57781, with some
-// addaptions to work with the new version of the base64 crate
-mod base64 {
-    use base64::alphabet;
-    use base64::engine::{GeneralPurpose, GeneralPurposeConfig};
-
-    const B64ENGINE: GeneralPurpose = base64::engine::general_purpose::GeneralPurpose::new(
-        &alphabet::STANDARD,
-        GeneralPurposeConfig::new(),
-    );
-
-    pub mod binary {
-        use super::B64ENGINE;
-        use base64::Engine;
-        use serde::{Deserialize, Serialize};
-        use serde::{Deserializer, Serializer};
-
-        pub fn serialize<S: Serializer>(v: &Vec<u8>, s: S) -> Result<S::Ok, S::Error> {
-            let base64 = B64ENGINE.encode(v);
-            String::serialize(&base64, s)
-        }
-
-        pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Vec<u8>, D::Error> {
-            let base64 = String::deserialize(d)?;
-            B64ENGINE
-                .decode(base64.as_bytes())
-                .map_err(serde::de::Error::custom)
-        }
-    }
-
-    pub mod optional_binary {
-        use super::B64ENGINE;
-        use base64::Engine;
-        use serde::{Deserialize, Serialize};
-        use serde::{Deserializer, Serializer};
-
-        pub fn serialize<S: Serializer>(v: &Option<Vec<u8>>, s: S) -> Result<S::Ok, S::Error> {
-            if let Some(v) = v {
-                let base64 = B64ENGINE.encode(v);
-                String::serialize(&base64, s)
-            } else {
-                <Option<String>>::serialize(&None, s)
-            }
-        }
-
-        pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Option<Vec<u8>>, D::Error> {
-            if let Some(base64) = <Option<String>>::deserialize(d)? {
-                B64ENGINE
-                    .decode(base64.as_bytes())
-                    .map_err(serde::de::Error::custom)
-                    .map(Option::Some)
-            } else {
-                Ok(None)
-            }
         }
     }
 }
