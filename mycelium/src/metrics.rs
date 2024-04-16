6aca671d68813fbb1830fb41d22d7954416e97b4
Lee Smet <lee.smet@hotmail.com>
2024-04-16 16:52:57 +0200

    Add initial router specific metrics
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- mycelium/src/metrics.rs
+++ mycelium/src/metrics.rs
@@ -14,4 +14,67 @@
 /// struct NoMetrics;
 /// impl Metrics for NoMetrics {}
 /// ```
-pub trait Metrics {}
+pub trait Metrics {
+    /// The [`Router`](crate::router::Router) received a new Hello TLV from a peer.
+    fn router_incoming_hello(&self) {}
+
+    /// The [`Router`](crate::router::Router) received a new IHU TLV from a peer.
+    fn router_incoming_ihu(&self) {}
+
+    /// The [`Router`](crate::router::Router) received a new Seqno request TLV from a peer.
+    fn router_incoming_seqno_request(&self) {}
+
+    /// The [`Router`](crate::router::Router) received a new Route request TLV from a peer.
+    /// Additionally, it is recorded if this is a wildcard request (route table dump request)
+    /// or a request for a specific subnet.
+    fn router_incoming_route_request(&self, _wildcard: bool) {}
+
+    /// The [`Router`](crate::router::Router) received a new Update TLV from a peer.
+    fn router_incoming_update(&self) {}
+
+    /// The [`Router`](crate::router::Router) received a new TLV from a peer, but the type is unknown.
+    fn router_incoming_unknown_tlv(&self) {}
+
+    /// A [`Peer`](crate::peer::Peer) was added to the [`Router`](crate::router::Router).
+    fn router_peer_added(&self) {}
+
+    /// A [`Peer`](crate::peer::Peer) was removed from the [`Router`](crate::router::Router).
+    fn router_peer_removed(&self) {}
+
+    /// A [`Peer`](crate::peer::Peer) informed the [`Router`](crate::router::Router) it died, or
+    /// the router otherwise noticed the Peer is dead.
+    fn router_peer_died(&self) {}
+
+    /// The [`Router`](crate::router::Router) ran a route selection procedure.
+    fn router_route_selection_ran(&self) {}
+
+    /// A [`SourceKey`](crate::source_table::SourceKey) expired and got cleaned up by the [`Router`](crate::router::Router).
+    fn router_source_key_expired(&self) {}
+
+    /// A [`RouteKey`](crate::routing_table::RouteKey) expired, and the router either set the
+    /// [`Metric`](crate::metric::Metric) of the route to infinity, or cleaned up the route entry
+    /// altogether.
+    fn router_route_key_expired(&self, _removed: bool) {}
+
+    /// A route which expired was actually the selected route for the
+    /// [`Subnet`](crate::subnet::Subnet). Note that [`Self::router_route_key_expired`] will
+    /// also have been called.
+    fn router_selected_route_expired(&self) {}
+
+    /// The [`Router`](crate::router::Router) sends a "triggered" update to it's peers.
+    fn router_triggered_update(&self) {}
+
+    /// The [`Router`](crate::router::Router) got a packet to route.
+    fn router_route_packet(&self) {}
+
+    /// The [`Router`](crate::router::Router) extracted a packet for the local subnet.
+    fn router_route_packet_local(&self) {}
+
+    /// The [`Router`](crate::router::Router) dropped a packet it was routing because it's TTL
+    /// reached 0.
+    fn router_route_packet_ttl_expired(&self) {}
+
+    /// The [`Router`](crate::router::Router) dropped a packet it was routing because there was no
+    /// route for the destination IP.
+    fn router_route_packet_no_route(&self) {}
+}
