9544fef2e4defd86bba96f02d365f847a4a34162
r-vdp <ramses@well-founded.dev>
2024-05-13 18:17:01 +0000

    Add an overlay to the flake

--- flake.nix
+++ flake.nix
@@ -12,45 +12,22 @@
 
   outputs =
     { self
-    , nixpkgs
     , crane
     , flake-utils
     , nix-filter
     , ...
-    }:
-    flake-utils.lib.eachSystem
-      [
-        flake-utils.lib.system.x86_64-linux
-        flake-utils.lib.system.aarch64-linux
-        flake-utils.lib.system.x86_64-darwin
-        flake-utils.lib.system.aarch64-darwin
-      ]
-      (system:
-      let
-        craneLib = crane.lib.${system};
+    }@inputs:
+    {
+      overlays.default = final: prev: {
+        mycelium =
+          let
+            craneLib = crane.lib.${final.system};
 
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit
-          (pkgs)
-          lib
-          stdenv
-          darwin
-          ;
+            inherit (final) lib stdenv darwin;
 
-        manifest = builtins.fromTOML (builtins.readFile ./mycelium/Cargo.toml);
-      in
-      {
-        devShells.default = craneLib.devShell {
-          packages = [
-            pkgs.rust-analyzer
-          ];
-
-          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-        };
-
-        packages = {
-          default = self.packages.${system}.mycelium;
-          mycelium = lib.makeOverridable craneLib.buildPackage {
+            manifest = builtins.fromTOML (lib.readFile ./mycelium/Cargo.toml);
+          in
+          lib.makeOverridable craneLib.buildPackage {
             src = nix-filter {
               root = ./.;
 
@@ -64,26 +41,57 @@
               ];
             };
             pname = manifest.package.name;
-            version = manifest.package.version;
+            inherit (manifest.package) version;
 
             doCheck = false;
 
             cargoExtraArgs = "--bin mycelium";
 
             nativeBuildInputs = [
-              pkgs.pkg-config
+              final.pkg-config
               # openssl base library
-              pkgs.openssl
+              final.openssl
               # required by openssl-sys
-              pkgs.perl
+              final.perl
             ];
 
             buildInputs = lib.optionals stdenv.isDarwin [
               darwin.apple_sdk.frameworks.Security
               darwin.apple_sdk.frameworks.SystemConfiguration
-              pkgs.libiconv
+              final.libiconv
             ];
           };
+      };
+    } //
+    flake-utils.lib.eachSystem
+      [
+        flake-utils.lib.system.x86_64-linux
+        flake-utils.lib.system.aarch64-linux
+        flake-utils.lib.system.x86_64-darwin
+        flake-utils.lib.system.aarch64-darwin
+      ]
+      (system:
+      let
+        craneLib = crane.lib.${system};
+
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+      in
+      {
+        devShells.default = craneLib.devShell {
+          packages = [
+            pkgs.rust-analyzer
+          ];
+
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+        };
+
+        packages = {
+          default = self.packages.${system}.mycelium;
+
+          inherit (pkgs) mycelium;
         };
       });
 }
