862249a0a73e6742ab206ef6c79593c66aaff3d8
Lee Smet <lee.smet@hotmail.com>
2025-09-17 12:26:58 +0200

    Implement actual proxy connection and forwarding
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- mycelium/src/proxy.rs
+++ mycelium/src/proxy.rs
@@ -1,18 +1,20 @@
 use std::{
     collections::HashMap,
-    net::Ipv6Addr,
-    sync::{Arc, RwLock},
+    net::{Ipv6Addr, SocketAddr},
+    sync::{Arc, Mutex, RwLock},
 };
 
 use crate::{metrics::Metrics, router::Router};
+use futures::stream::FuturesUnordered;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpStream,
+    net::{TcpListener, TcpSocket, TcpStream},
     select,
     time::{self, timeout, Duration},
 };
+use tokio_stream::StreamExt;
 use tokio_util::sync::CancellationToken;
-use tracing::{debug, info, trace};
+use tracing::{debug, error, info, trace, warn};
 
 /// Amount of time to wait between probes for proxies.
 const PROXY_PROBE_INTERVAL: Duration = Duration::from_secs(60);
@@ -44,7 +46,11 @@ const SOCKS5_SERVER_CHOICE_DENIED: [u8; 2] = [
 pub struct Proxy<M> {
     router: Router<M>,
     proxy_cache: Arc<RwLock<HashMap<Ipv6Addr, ProxyProbeStatus>>>,
-    cancel_token: CancellationToken,
+    chosen_remote: Arc<Mutex<Option<SocketAddr>>>,
+    /// Cancellation token used for scanning routines
+    scan_token: Arc<Mutex<CancellationToken>>,
+    /// Cancellation token used for the actual proxy connections
+    proxy_token: Arc<Mutex<CancellationToken>>,
 }
 
 /// Status of a probe for a proxy.
@@ -70,7 +76,9 @@ where
         Self {
             router,
             proxy_cache: Arc::new(RwLock::new(HashMap::new())),
-            cancel_token: CancellationToken::new(),
+            chosen_remote: Arc::new(Mutex::new(None)),
+            scan_token: Arc::new(Mutex::new(CancellationToken::new())),
+            proxy_token: Arc::new(Mutex::new(CancellationToken::new())),
         }
     }
 
@@ -84,7 +92,7 @@ where
         info!("Start Socks5 proxy probing");
         let router = self.router.clone();
         let proxy_cache = self.proxy_cache.clone();
-        let cancel_token = self.cancel_token.clone();
+        let cancel_token = self.scan_token.lock().expect("Can lock cancel_token; qed").clone();
 
         tokio::spawn(async move {
             loop {
@@ -107,7 +115,7 @@ where
 
                         debug!(%address, "Probing Socks5 proxy");
 
-                       if let Err(_) =  
+                       if   
                             timeout(PROBE_TIMEOUT, async {
                                 let mut stream =
                                     match TcpStream::connect((address, DEFAULT_SOCKS5_PORT)).await {
@@ -171,7 +179,7 @@ where
                                     }
                                 }
                             })
-                            .await {
+                            .await.is_err() {
                                 debug!(%address, "Timeout probing proxy");
                                 proxy_cache
                                     .write()
@@ -187,6 +195,142 @@ where
     /// Stops any ongoing probes. 
     pub fn stop_scanning(&self) {
         info!("Stopping Socks5 proxy probing");
-        self.cancel_token.cancel();
+        self.scan_token.lock().expect("Can lock cancel token; qed").cancel();
+    }
+
+    /// Connect to a remote Socks5 proxy. If a proxy address is given, connect to that one. If not, connect to the best (fastest) known proxy.
+    pub async fn connect(&self, remote: Option<SocketAddr>) -> Result<SocketAddr, ConnectionError> {
+        let target = match remote {
+            Some(remote) => remote,
+            None => {
+                debug!("Finding best known proxy");
+                // Find best proxy of our internal list by racing all proxies and finding the first
+                // one which gives a valid response.
+                let futs = FuturesUnordered::new();
+                for ip in self.proxy_cache.read().expect("Can read lock proxy cache; qed").iter().filter_map(|(address, ps)| {
+                    if matches!(ps, ProxyProbeStatus::Valid) {
+                        Some(*address)
+                    } else {
+                        None
+                    }
+                }) {
+                    futs.push(async move {
+                        // It's fine to swallow errors here, we are just sanity checking
+                        let addr: SocketAddr = (ip, DEFAULT_SOCKS5_PORT).into();
+                        trace!(%addr, "Checking proxy availability and latency");
+                        let mut stream = TcpStream::connect(addr).await.ok()?;
+                        stream.write_all(&SOCKS5_CLIENT_GREETING).await.ok()?;
+                        let mut recv_buf = [0;2];
+                        stream.read_exact(&mut recv_buf).await.ok()?;
+                        match recv_buf {
+                            SOCKS5_EXPECTED_SERVER_CHOICE => Some(addr),
+                            _ => None
+                        }
+                    });
+                }
+
+                let target: Option<SocketAddr> = futs.filter_map(|o| o).next().await;
+                if target.is_none() {
+                    return Err(ConnectionError{_private: ()})
+                }
+                // Safe since we just checked the None case above
+                target.unwrap()
+            }
+        };
+
+        // Now that we have a target, "connect" to it, i.e. set it as proxy destination.
+        debug!(%target, "Setting remote Socks5 proxy");
+        *self.chosen_remote.lock().expect("Can lock chosen remote; qed") = Some(target);
+
+
+        Ok(target)
+    }
+
+    /// Disconnects from the proxy, if any is connected
+    pub async fn disconnect(&self) {
+        self.proxy_token.lock().expect("Can lock proxy token; qed").cancel();
+        *self.chosen_remote.lock().expect("Can lock chosen remote; qed") = None;
+
+    }
+
+    /// Starts a background task for proxying connections.
+    /// This spawns a listener, and proxies all connections to the chosen target.
+    fn start_proxy(&self) {
+        let target = *self.chosen_remote.lock().expect("Can lock chosen remote; qed");
+        // First cancel the old token, then set a new token
+        let mut old_token = self.proxy_token.lock().expect("Can lock proxy token; qed");
+        old_token.cancel();
+        let proxy_token = CancellationToken::new();
+        *old_token = proxy_token.clone();
+
+        tokio::spawn(async move {
+            let listener = TcpListener::bind(("[::]", DEFAULT_SOCKS5_PORT)).await?;
+
+            loop {
+                select! {
+                    _ = proxy_token.cancelled() => {
+                        debug!("Shutting down proxy listener");
+                        break
+                    }
+                    stream = listener.accept() => {
+                        match stream {
+                            Err(err) => {
+                                error!(%err, "Proxy listener accept error");
+                                return Err(err)
+                           }
+                            Ok((mut stream, source)) => {
+                                trace!(%source, "Got new proxy stream");
+                                let proxy_token = proxy_token.clone();
+                                let target = target;
+                                if target.is_none() {
+                                    warn!("Refusing to proxy Socks5 stream as we have no remote set - this should not happen");
+                                    break
+                                }
+                                // Unwrap is safe since we checked the none variant above
+                                let target = target.unwrap();
+                               tokio::spawn(async move {
+                                    let mut con = TcpStream::connect(target).await?;
+                                    select! {
+                                        _ = proxy_token.cancelled() => {
+                                            trace!(%source, %target, "Shutting down proxy stream");
+                                            Ok(())
+                                        }
+                                        r = tokio::io::copy_bidirectional(&mut stream, &mut con) => {
+                                            match r {
+                                                Err(err) => {
+                                                    trace!(%err, %source, %target, "Proxy stream finished with error");
+                                                    Err(err)
+                                                }
+                                                Ok((_, _)) => {
+                                                    trace!(%source, %target, "Proxy stream finished normally");
+                                                    Ok(()) 
+                                                }
+                                            }
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+
+            Ok(())
+        });
     }
 }
+
+/// Error returned when performing an automatic Socks5 connect, but no valid remotes are found.
+#[derive(Debug)]
+pub struct ConnectionError {
+    _private: (),
+
+}
+
+impl std::fmt::Display for ConnectionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("No valid Socks5 proxy found to connect to")
+    }
+}
+
+impl std::error::Error for ConnectionError {}
