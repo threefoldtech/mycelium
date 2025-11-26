13672bbcbf225fd06eb34bd2a9f48f6d7ed945ce
Lee Smet <lee.smet@hotmail.com>
2025-11-26 10:32:25 +0100

    Revert "Use BBR congestion controller for quic"
    This reverts commit f68b8823bbaab41040f5f0ded6f7d67af0b66b91.
    
    BBR congestion controller implementation in quinn is currently not
    maintained

--- mycelium/src/peer_manager.rs
+++ mycelium/src/peer_manager.rs
@@ -9,7 +9,7 @@ use futures::{FutureExt, StreamExt};
 #[cfg(feature = "private-network")]
 use openssl::ssl::{Ssl, SslAcceptor, SslConnector, SslMethod};
 use quinn::crypto::rustls::QuicClientConfig;
-use quinn::{congestion, MtuDiscoveryConfig, ServerConfig, TransportConfig};
+use quinn::{MtuDiscoveryConfig, ServerConfig, TransportConfig};
 use rustls::pki_types::{CertificateDer, PrivatePkcs8KeyDer, ServerName, UnixTime};
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
@@ -1222,7 +1222,6 @@ fn make_quic_endpoint(
     transport_config.datagram_send_buffer_size(16 << 20);
     transport_config.initial_mtu(1500);
     transport_config.enable_segmentation_offload(true);
-    transport_config.congestion_controller_factory(Arc::new(congestion::BbrConfig::default()));
     // TODO: further tweak this.
 
     let socket = std::net::UdpSocket::bind(("::", quic_listen_port))
