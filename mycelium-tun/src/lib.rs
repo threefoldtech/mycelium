511cf45e8510548880530e7d75b9d5138eec738a
Lee Smet <lee.smet@hotmail.com>
2026-02-24 11:22:46 +0100

    Add custom tun implementation on linux
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

---
+++ mycelium-tun/src/lib.rs
@@ -0,0 +1,9 @@
+//! Platform-specific TUN device implementations.
+//!
+//! Currently only Linux is supported. On other platforms this crate is empty.
+
+#[cfg(target_os = "linux")]
+mod linux;
+
+#[cfg(target_os = "linux")]
+pub use linux::Tun;
