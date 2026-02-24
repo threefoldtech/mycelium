0243feaa3dfd96445c82baa4e1e930e5ee78fd66
Lee Smet <lee.smet@hotmail.com>
2026-02-24 11:22:46 +0100

    Add custom tun implementation on linux
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -41,6 +41,7 @@
                   ./mycelium-api
                   ./mycelium-cli
                   ./mycelium-metrics
+                  ./mycelium-tun
                   ./myceliumd
                   ./myceliumd-common
                   ./myceliumd-private
@@ -88,6 +89,7 @@
                   ./mycelium-api
                   ./mycelium-cli
                   ./mycelium-metrics
+                  ./mycelium-tun
                   ./myceliumd
                   ./myceliumd-common
                   ./myceliumd-private
