b657d9fe8253828d554161e1a1da126c718dbaf1
Lee Smet <lee.smet@hotmail.com>
2026-02-26 15:54:49 +0100

    Implement GRO/GSO (TSO/USO 4/6) in linux tun crate
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- mycelium-tun/src/lib.rs
+++ mycelium-tun/src/lib.rs
@@ -3,7 +3,11 @@
 //! Currently only Linux is supported. On other platforms this crate is empty.
 
 #[cfg(target_os = "linux")]
+mod checksum;
+#[cfg(target_os = "linux")]
 mod linux;
+#[cfg(target_os = "linux")]
+mod offload;
 
 #[cfg(target_os = "linux")]
 pub use linux::{ReadHalf, Tun, WriteHalf};
